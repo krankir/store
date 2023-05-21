6737ebc67054951e551bb44f08fc1b9e7a476b72
Анатолий Редько <stroitel.proekt@gmail.com>
2023-05-21 23:31:32 +0500

    add login and registration

--- store/users/views.py
+++ store/users/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
 
-# Create your views here.
+from users.forms import UserLoginForm, UserRegistrationForm
+
+User = get_user_model()
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('products:index'))
+
+    else:
+        form = UserLoginForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'users/login.html', context)
+
+
+def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'users/registration.html', context)
+
